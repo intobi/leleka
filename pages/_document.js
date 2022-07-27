f17758c1b3dffaab6a56f8528981b8a5b5a61715
ArthurSnoz <a.snoz@intobi.com>
2022-07-27 22:15:12 +0300

    added og:image

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,8 @@ const Document = () => {
                 <link
                     href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
                     rel="stylesheet"/>
+                <meta property="og:image" content='/facebook.png' key='image'/>
+                <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
             </Head>
             <body>
             <Main/>
